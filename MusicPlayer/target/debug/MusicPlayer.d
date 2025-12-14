4afb679cac2c6042ec89be81b04f7647c6220553
Zach Yates <zyates@student.cvtc.edu>
2025-12-14 14:23:34 -0600

    initial setup

---
+++ MusicPlayer/target/debug/MusicPlayer.d
@@ -0,0 +1,1 @@
+C:\Users\22yat\RustMusicApp\MusicPlayer\target\debug\MusicPlayer.exe: C:\Users\22yat\RustMusicApp\MusicPlayer\src\main.rs
---
+++ MusicPlayer/target/debug/deps/MusicPlayer.d
@@ -0,0 +1,5 @@
+C:\Users\22yat\RustMusicApp\MusicPlayer\target\debug\deps\MusicPlayer.d: src\main.rs
+
+C:\Users\22yat\RustMusicApp\MusicPlayer\target\debug\deps\MusicPlayer.exe: src\main.rs
+
+src\main.rs:
